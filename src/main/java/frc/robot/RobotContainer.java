988c5de79d083718f36d3e4c1986c49463557796
MrFurbie <jayjayrebruf@gmail.com>
2024-12-13 11:59:59 -0800

    Created and tested first auto mode using choreo, robot behaved as expected

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,12 +1,18 @@
 package frc.robot;
 
+import com.choreo.lib.Choreo;
+import com.choreo.lib.ChoreoTrajectory;
 import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -20,6 +26,18 @@ public class RobotContainer {
                                                                      
   private double MaxAngularRate = 1.7 * Math.PI; // Controls how fast the robot quick turns
 
+  public boolean isRed() {
+
+    if (DriverStation.getAlliance().isPresent()) {
+
+      return DriverStation.getAlliance().get() ==  Alliance.Red;
+
+    }
+
+    return false;
+
+  }
+
   public double getVoltage() {
 
     return  
@@ -28,7 +46,7 @@ public class RobotContainer {
 
   }
 
-    public double getVelocity() {
+  public double getVelocity() {
 
     return  
 
@@ -44,7 +62,9 @@ public class RobotContainer {
       .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.05) // Add a 5% deadband
       .withDriveRequestType(DriveRequestType.Velocity); 
 
-  private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+  private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake(); 
+
+  private final SwerveRequest.ApplyChassisSpeeds chassisSpeedRequest = new SwerveRequest.ApplyChassisSpeeds();
 
   private final Telemetry logger = new Telemetry(MaxSpeed);
 
@@ -82,9 +102,45 @@ public class RobotContainer {
 
   }
 
+  PIDController xPID = new PIDController(3, 0, 0);
+  PIDController yPID = new PIDController(3, 0, 0);
+  PIDController rPID = new PIDController(3, 0, 0);
+
+  public void runChassisSpeeds(ChassisSpeeds speeds){ 
+
+    drivetrain.setControl(chassisSpeedRequest.withSpeeds(speeds));
+
+  }
+
+  public Command runChoreoPath(String pathName, boolean resetPose) {
+  
+    ChoreoTrajectory trajectory = Choreo.getTrajectory(pathName);
+
+    rPID.enableContinuousInput(-Math.PI, Math.PI);
+
+    Command setPose = Commands.either(
+      Commands.runOnce(() -> drivetrain.seedFieldRelative(isRed() ? 
+      trajectory.flipped().getInitialPose() : trajectory.getInitialPose()), drivetrain), 
+      Commands.none(), () -> resetPose);
+
+    Command autoCommand = Choreo.choreoSwerveCommand(
+        trajectory,
+        () -> drivetrain.getState().Pose,
+        xPID,
+        yPID,
+        rPID,
+        this::runChassisSpeeds,
+        this::isRed
+
+    );
+    
+    return Commands.sequence(setPose, autoCommand);
+
+  }
+
   public Command getAutonomousCommand() {
 
-    return Commands.print("No autonomous command configured");
+    return runChoreoPath("Test", true);
 
   }
 
